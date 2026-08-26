fb1fb2bacea5a1f3b9c312bf1750f4448422481e
jaymbans <jordan@mbanefo.com>
2026-08-26 15:59:02 -0400

    implement date and category filtering

--- cookiecutter_django_vector_dbs/search/api/views.py
+++ cookiecutter_django_vector_dbs/search/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
+import datetime
 
 from cookiecutter_django_vector_dbs.search.embeddings import create_embeddings
 from cookiecutter_django_vector_dbs.search.models import Document
@@ -18,14 +19,34 @@ class DocumentViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["post"])
     def search(self, request):
         query = request.data.get("query", "")
+        min_date = request.data.get("min_date", "")
+        max_date = request.data.get("max_date", "")
+        category = request.data.get("category", "")
 
         if not query:
             raise ValidationError("query is required")
+
+        date_filter_valid = min_date and max_date
+        date_exists = min_date
+
+        if date_exists and not date_filter_valid:
+            raise ValidationError("include min and max date or neither (YYYYMMDD)")
+
+        docs = Document.objects.all()
+
+        if date_exists:
+            docs = docs.filter(published_date__gt=min_date).filter(published_date__lt=max_date)
+
+        if category:
+            docs = docs.filter(category=category)
+
         query_embedding = create_embeddings([query])[0]
 
         docs = DocumentSearchResultSerializer(
-            Document.objects.annotate(distance=CosineDistance("embedding", query_embedding)).order_by("distance")[:3],
+            docs.annotate(distance=CosineDistance("embedding", query_embedding)).order_by("distance")[:3],
             many=True
         )
+
+
 
         return Response(docs.data)
